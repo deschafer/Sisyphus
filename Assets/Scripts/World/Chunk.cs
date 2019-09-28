806fa9b2e025abb26c78d49084d90567f77b3a3a
Graeme Holliday <shadowsofundin@gmail.com>
2019-09-27 21:29:36 -0700

    Revert "first take on world generation"
    This reverts commit e86995755b28863813875f79a30b41236652c409.

--- Assets/Scripts/World/Chunk.cs
+++ Assets/Scripts/World/Chunk.cs
@@ -4,12 +4,9 @@ using UnityEngine;
 
 public class Chunk {
 
-	public const int CHUNK_WIDTH = 16;
-	public const int CHUNK_HEIGHT = 32;
-
     private BiomeBase biome;
 
-    public Chunk(int[,] grid) {
+    public Chunk() {
 
     }
 
