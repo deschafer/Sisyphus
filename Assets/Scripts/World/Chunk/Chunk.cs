3f0eeefdbb8feb002efeb03fd59a4b97e8719c5d
Graeme Holliday <shadowsofundin@gmail.com>
2019-10-06 14:58:14 -0700

    add docs

--- Assets/Scripts/World/Chunk/Chunk.cs
+++ Assets/Scripts/World/Chunk/Chunk.cs
@@ -2,10 +2,11 @@
 
 public class Chunk {
 
-	public const int CHUNK_WIDTH = 128;
+	public const int CHUNK_WIDTH = 32;
 	public const int CHUNK_HEIGHT = 32;
 
     private BiomeBase biome;
+	private int positionX;
 
 	//REMOVE THIS
 	public int[,] tempGrid;
@@ -13,5 +14,9 @@ public class Chunk {
     public Chunk(int[,] grid) {
 		tempGrid = grid;
     }
+
+	public int getX() {
+		return positionX;
+	}
 
 }
