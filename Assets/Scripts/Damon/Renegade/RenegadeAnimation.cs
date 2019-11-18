398ef7efa5283ed419e9ec8419e21514b57e6197
deschafer <colonelschafer@gmail.com>
2019-11-18 10:27:48 -0800

    ...

---
+++ Assets/Scripts/Damon/Renegade/RenegadeAnimation.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RenegadeAnimation : EnemyBehavior
+{
+
+	public RenegadeAnimation(Enemy enemy) : 
+		base(enemy)
+	{
+	}
+
+	public override bool Act()
+	{
+
+		// if the player is moving
+		if (ParentEntity.State == Enemy.EnemyState.PATROL)
+		{
+			// if the enemy has jumped and is in the air
+			if (!ParentEntity.IsGrounded())
+			{
+				ParentEntity.Animator.SetBool("Jump", true);
+				ParentEntity.Animator.SetBool("Run", false);
+			}
+			else
+			{
+				ParentEntity.Animator.SetBool("Run", true);
+				ParentEntity.Animator.SetBool("Jump", false);
+			}
+		}
+		// if the renegade is not moving
+		else if (ParentEntity.State == Enemy.EnemyState.IDLE)
+		{
+			ParentEntity.Animator.SetBool("Run", false);
+			ParentEntity.Animator.SetBool("Idle", true);
+		}
+
+
+
+		return true;
+	}
+}
---
+++ Assets/Scripts/Mary/PowerUp.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUp : MonoBehaviour
+{
+    public int Max_Health = 100; 
+    public int healthboost;
+    public int speedboost;
+    public int attackboost;
+    public int jumpboost;
+    public PlayerHealth playerstats; //connects to the player manager
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerstats = FindObjectOfType<PlayerHealth>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+}
