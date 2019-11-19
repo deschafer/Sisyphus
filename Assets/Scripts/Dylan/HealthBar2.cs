2678ed1e68f4567b0df7f9296e57044dd6175bd0
Card0899 <54822419+Card0899@users.noreply.github.com>
2019-11-18 18:26:45 -0800

    ....

--- Assets/Scripts/Dylan/HealthBar2.cs
+++ Assets/Scripts/Dylan/HealthBar2.cs
@@ -8,12 +8,21 @@ public class HealthBar2 : MonoBehaviour
     // Start is called before the first frame update
     private Health2 health;
     private Image TopLayerImage;
-    
+    /* Health2 is the class that dictates the logic behind the health bar functioning.  It is a singleton pattern ans thus was made into its own class.
+     * The TopLayerImage is the image of the top layer of the health bar.  The portion of the health bar that is filled and unfilled according to the 
+     * change in the players health. 
+     * The data type Image is a standard Unity type.
+     * */
+
     private void Awake()
     {
         TopLayerImage = transform.Find("TopLayer").GetComponent<Image>();
         health = new Health2();
     }
+    /* The TopLayerImage is set to the image component of the top layer of the health
+     * health is being set to a new health2 class.  The design of the health 2 class will either return the only current
+     * version of the health2 class or it will create one if there is not already one created.
+     */
 
     // Update is called once per frame
     void Update()
@@ -22,6 +31,10 @@ public class HealthBar2 : MonoBehaviour
         TopLayerImage.fillAmount = health.NormalizeHealth();
     }
 }
+/*The update function called the normalize health function to decide how far to fill the health bar. 
+     * It divides the total current health of the player by the max total health for the player which returns a
+     * normalized version of the health. (ie. 10/100 yields 0.1) The fill amount of the toplayer image must be a number between 0 and 1
+     */
 
 
 public sealed class Health2
@@ -30,10 +43,15 @@ public sealed class Health2
     private float healthTotal;
     private static Health2 instance = null;
     private static readonly object padlock = new object();
-    
+    /* Health_Max is the total maximum health that the player can have.  It is used to normalize the health value of the player (ie. between 0 and 1)
+     * the health total is pulled from the PlayerHealth script and updated once per frame
+     * The Health2 instance is one of the mechanisms used to ensure that only one instance of the class is generated.
+     * The padlock object is used to lock and unlock the generation function for the health2 class.
+     */
+
     public Health2()
     {
-
+        
     }
     
     public static Health2 Instance
@@ -54,12 +72,20 @@ public sealed class Health2
             }
         }
     }
+
+    /* Instance is used to generate one and only one instance of the class health2.  This is done by checking for an instance of the class
+     * and return the instance if it exists. Otherwise it generates one instance ofthe class, sets the instance variable to the instance of the class, then
+     * returns that instance of the class the the function that called
+     */
     public void Update()
     {
 
     }
+    /* The update function is left blank as nothing needs to be done for the update but it inherits an update from the superclass
+     * */
     public float NormalizeHealth()
     {
         return healthTotal / Health_Max;
     }
+    /* Normalizehealth normalizes the health total of the player such that it is a number between 0 and 1
 }
