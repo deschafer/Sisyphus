490b54582b7298950b74157af03384c903e8458b
Card0899 <54822419+Card0899@users.noreply.github.com>
2019-11-18 17:14:38 -0800

    ...

---
+++ Assets/Scripts/Dylan/TRDNT_PU.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TRDNT_PU : PU_HUD_HANDLER
+{
+    private TimeOut trident;
+    //public Text timer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        trident = FindObjectOfType<TimeOut>();
+        powerUp.color = new Color(1f, 1f, 1f, 0.0f);
+
+    }
+
+    // Update is called once per frame
+    public override void Update()
+    {
+        if (trident.type == 3)
+        {
+            if (trident.timerActive > 0.0f)
+            {
+                float step = duration * Time.deltaTime;
+                powerUp.color = new Color(1f, 1f, 1f, Mathf.Lerp(powerUp.color.a, maxAlpha, step));
+                timer.color = new Color(1f, 1f, 1f, Mathf.Lerp(timer.color.a, maxAlpha, step));
+            }
+            else if (trident.timerActive <= 0.0f)
+            {
+                float step = duration * Time.deltaTime;
+                powerUp.color = new Color(1f, 1f, 1f, Mathf.Lerp(powerUp.color.a, minAlpha, step));
+                timer.color = new Color(1f, 1f, 1f, Mathf.Lerp(timer.color.a, minAlpha, step));
+            }
+        }
+    }
+    /*public override void powerUpTimer()
+    {
+      //  throw new System.NotImplementedException();
+    }*/
+}
