bfd479f459d7d9b85aa07c344ca01008300c0956
deschafer <colonelschafer@gmail.com>
2019-11-18 17:28:15 -0800

    ...

--- Assets/Scripts/Dylan/PU_HUD_HANDLER.cs
+++ Assets/Scripts/Dylan/PU_HUD_HANDLER.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public abstract class PU_HUD_HANDLER : MonoBehaviour
 {
@@ -8,6 +9,8 @@ public abstract class PU_HUD_HANDLER : MonoBehaviour
     public const float minAlpha = 0.00f;
     public const float duration = 3.00f;
     public int flag = 1;
+    public Text timer;
+    public TimeOut timeOut;
 
     public SpriteRenderer powerUp;
 
@@ -15,9 +18,13 @@ public abstract class PU_HUD_HANDLER : MonoBehaviour
     void Start()
     {
         powerUp.color = new Color(1f, 1f, 1f, 0.0f);
+        timeOut = FindObjectOfType<TimeOut>();
     }
 
     // Update is called once per frame
     public abstract void Update();
-    public abstract void powerUpTimer();
+    /*public void powerUpTimer()
+    {
+        timer.text = " " + timeOut.timerActive;
+    }*/
 }
