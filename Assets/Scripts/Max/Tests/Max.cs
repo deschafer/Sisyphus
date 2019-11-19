8daae58c2727de2b2051ba161875f23d9ea447d5
moor9140 <54828776+moor9140@users.noreply.github.com>
2019-11-19 13:51:37 -0800

    update docs

--- Assets/Scripts/Max/Tests/Max.cs
+++ Assets/Scripts/Max/Tests/Max.cs
@@ -3,17 +3,21 @@ using System.Collections.Generic;
 using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.TestTools;
-
+/*
+author: Max Moore
+purpose: Initiate tests for the test runner, find test cases
+parameters: a working game scene
+*/
 namespace Tests
 {
     public class Max
 	{
         [Test]
-        public void PowerupSpawned()
+        public void PlayerSpawned()
         {
             Vector3 PlayerPosition = GameObject.Find("Player").transform.position;
             Assert.IsNotNull(PlayerPosition);
-            //Debug.Log("Powerup did not spawn");
+            //Debug.Log("Player did not spawn");
         }
 
     }
